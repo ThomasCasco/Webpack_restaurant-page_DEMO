b188faa577f6da4fd503ebd0ea467024e79fff20
Thomas <thomas.s.casco@gmail.com>
2021-10-06 19:22:03 -0300

    all the code

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import './style.css';
+const contenido = document.querySelector('#content');
+
+
+contenido.innerHTML = `<header>
+        
+<h1>Restaurante La cacerola</h1>
+<nav>
+    <a href="">about us</a>
+    <a href="">contact</a>
+    <a href="">menu</a>
+</nav>
+
+</header>
+
+<main>
+<section>
+    <div>
+    <h1>Pechuga empanizada la cacerola</h1>    
+    <img src="../src/pechuga+empanizada.jpg" alt="pechuga empanizada">
+    </div>
+</section>
+<section>
+    <div>
+        <h1>nosotros</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam aliquam iste cumque, mollitia impedit nihil. Quam perspiciatis a quia eveniet accusamus sint similique nemo culpa? Commodi et aut itaque ea?</p>
+    </div>
+</section>
+</main>`
